b5a6b22e015d9e5cbce663812c99cf254bd00f22
astroProject <akshayhemant@hotmail.com>
2020-07-22 07:29:19 +0530

    Revert "Added Guess the number"
    This reverts commit d2aa4de22f929dd5540299e46b85b847b76121ae.

--- secondappp/App.js
+++ secondappp/App.js
@@ -1,25 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen';
+
 export default function App() {
-  const [userNumber, setUserNumber] = React.useState();
-
-  const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber);
-  };
-
-  let content = <StartGameScreen startGameHandler={startGameHandler} />;
-
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
-  }
-
   return (
     <View style={styles.container}>
-      <Header title='Guess the number' />
-      {content}
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -28,5 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
